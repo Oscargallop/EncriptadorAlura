a2b15587eb9e74d89807d4882f16d5ad809c5583
Oscargallop <Oscargallop@gmail.com>
2024-08-14 00:21:54 -0500

    Comenzamos a darle forma al Htlm, Style y el esqueleto de del JS

---
+++ Encripator.js
@@ -0,0 +1,25 @@
+
+
+function textoEncriptar () {
+
+}
+
+function botonCifrar () {
+
+}
+
+function cifrar () {
+
+}
+
+function botonDescifrar () {
+
+}
+
+function descifrar () {
+
+}
+
+function copiar () {
+    
+}
