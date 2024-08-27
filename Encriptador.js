b39f26738943940a69017fe95c6069c5556c1b42
Oscargallop <Oscargallop@gmail.com>
2024-08-26 23:23:38 -0500

    Funcionando para entregar

--- Encriptador.js
+++ Encriptador.js
@@ -1,7 +1,19 @@
 //let textoUsuario = 'abcd';
 let arrayEncriptado = [];
+let arrayDesencriptado = [];
+let textoEncriptadoParaMostrar = "";
 //const salidaMensaje = document.querySelector(".cuadro_salida")
+//let textoUsuario = "";
 
+function botonEncriptar () {    
+    const textoEncriptadoParaMostrar = cifrar();
+    //let paraBorrar = document.getElementById("textoUsuario").value;
+    //cuadro_salida.value = textoEncriptadoParaMostrar;
+    //cuadro_salida.style.backgroundImage = 
+    //document.getElementById('img').style.display = "none";
+    console.log(textoEncriptadoParaMostrar);
+    return;
+}
 
 function cifrar () {
     let paraEncriptar = document.getElementById("textoUsuario").value;
@@ -36,26 +48,69 @@ function cifrar () {
     //console.log(arrayEncriptado);
     const textoEncriptado = arrayEncriptado.join("");
     console.log(textoEncriptado);
+    //
     //salidaMensaje.value = textoEncriptado
     document.getElementById("textoEncriptadoMensaje").innerText = textoEncriptado;
+    document.getElementById("textoUsuario").value = "";
+    return arrayEncriptado;
+}
+
+function copiarTexto () {
+    document.getElementById.textoEncriptadoParaMostrar = document.getElementById(textoEncriptadoMensaje).value;
+    console.log(textoEncriptadoParaMostrar);
     return;
 }
+
+
+function botonDesencriptar () {
+    //let arrayEncriptado = arrayEncriptado;
+    //console.log(arrayEncriptado.length);
+    longitud2 = arrayEncriptado.length;
+    console.log(longitud2);
+    //let paraDesencriptar = document.getElementById(textoEncriptado).value;
+    //let longitud = arrayEncriptado.length;
+    //console.log("la longitud es", longitud);
+    //const mensaje = new String (paraDesencriptar);
+    //console.log("la letra es", letra);
+    for(let i = 0; i < longitud2; i ++){
+        arr = arrayEncriptado[i];
+        if(arr == "ai"){
+            arrayDesencriptado[i] = "a";
+        } else {
+            if(arr == "enter"){
+                arrayDesencriptado[i] = "e";
+            } else {
+                if(arr == "imes"){
+                    arrayDesencriptado[i] = "i";
+                } else {
+                    if(arr == "ober"){
+                        arrayDesencriptado[i] = "o";
+                    } else {
+                        if(arr == "ufat"){
+                            arrayDesencriptado[i] = "u";
+                        } else {
+                            arrayDesencriptado[i] = arr;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    console.log (arrayDesencriptado);
+    
+    //console.log(arrayEncriptado);
+    const textoDesencriptado = arrayDesencriptado.join("");
+    console.log(textoDesencriptado);
+    //reversaMensaje.value = textoDesencriptado
+    //document.getElementById("textoUsuario").innerText = textoDesencriptado;
+    document.getElementById("textoEncriptadoMensaje").value = textoDesencriptado;
+    return textoDesencriptado;
+}
+
 
 /*
 function verificador (str){
     
 }
 
-/*
-function botonDescifrar () {    
-
-}
-
-function descifrar () {
-
-}
-
-function copiar () {
-    
-}
 */
